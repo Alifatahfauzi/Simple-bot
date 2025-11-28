7757e640c8758f9ac623065075f4f82da6926634
i`m fauzialifatah <118640580+Alifatahfauzi@users.noreply.github.com>
2025-11-28 18:49:20 +0900

    Fauzialifatah

---
+++ settings/config.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import chalk from "chalk";
+
+/** info id **/
+global.owner = ["6281340019858","226581553406099@lid"];
+global.mode = false;
+
+/** nama bot **/
+global.namebotz = "Alifatah wabot !";
+global.packname = 'www.ziihost.store';
+global.nameown = "Fauzialifatah | Projects";
+global.author = 'https://www.github.com/Alifatahfauzi';
+global.footer = "𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆: @FauziAlifatah";
+
+/** media **/
+global.YouTube = "https://www.youtube.com/@Fauzialifatah";
+global.GitHub = "https://github.com/Alifatahfauzi";
+global.Telegram = "https://t.me/FauziAlifatah";
+global.ChannelWA = "https://whatsapp.com/channel/0029VawsCnQ9mrGkOuburC1z";
+
+/** message **/
+global.mess = {
+    group: "ngapain? khusus grup njrr",
+    admin: "ngapain? khusus admin njrr",
+    owner: "apalah, bukan owner",
+    botadmin: "bot bukan admin"
+}
