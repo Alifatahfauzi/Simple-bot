96d10f73a570af19dd4b2a2e713615bd4066f8f2
i`m fauzialifatah <118640580+Alifatahfauzi@users.noreply.github.com>
2026-01-11 02:41:19 +0000

    Fauzialifatah
    Upgrade

---
+++ settings/config.js
@@ -0,0 +1,45 @@
+import fs from "fs";
+import chalk from "chalk";
+
+/** info id **/
+global.owner = ["6282199509537","210780402241764@lid"];
+global.mode = false;
+
+/** apikey **/
+global.velynApiKey = "-";
+global.anaApiKey = "-";
+global.termaiApikey = "-";
+
+/** pairing Code Settings **/
+global.pairingPhoneNumber = "6285211997125"; 
+global.customPairingCode = "12345678"; 
+
+/** settings panel **/
+global.egg = "15"; // Isi id egg
+global.nestid = "5"; // Isi id nest
+global.loc = "1"; // Isi id location
+global.domain = ""
+global.apikey = ""; // Isi api ptla
+global.capikey = ""; // Isi api ptlc
+
+/** nama bot **/
+global.namebotz = "rche x pauzi";
+global.packname = 'www.ziihost.store';
+global.nameown = "Fauzialifatah | Projects";
+global.author = 'https://www.github.com/Alifatahfauzi';
+global.footer = "𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆: @FauziAlifatah";
+
+/** media **/
+global.YouTube = "https://www.youtube.com/@Fauzialifatah";
+global.GitHub = "https://github.com/Alifatahfauzi";
+global.Telegram = "https://t.me/FauziAlifatah";
+global.ChannelWA = "https://whatsapp.com/channel/0029VawsCnQ9mrGkOuburC1z";
+
+/** message **/
+global.mess = {
+    group: "ngapain? khusus grup njrr",
+    admin: "ngapain? khusus admin njrr",
+    private: "ngapain? khusu private njirr",
+    owner: "apalah, bukan owner",
+    botadmin: "bot bukan admin"
+}
